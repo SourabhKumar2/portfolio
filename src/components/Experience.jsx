b60ad1ec15242b374543606cb6d9db1fb7edf8e8
sourabhkumar2 <sourabhkumar2405@gmail.com>
2022-02-12 16:33:43 +0530

    refactor

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,83 +1,59 @@
 export const Experience = () => {
+    const experiences = [
+        {
+            image: "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100",
+            date: "Jan 2022",
+            heading: "An Amazing website",
+            content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ullam ipsam. At cupiditate accusantium optio repudiandae pariatur fugiat necessitatibus, aut doloremque saepe ex iste rem distinctio veniam doloribus aliquid dolor."
+        },
+        {
+            image: "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100",
+            date: "Jan 2021",
+            heading: "An Amazing website",
+            content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ullam ipsam. At cupiditate accusantium optio repudiandae pariatur fugiat necessitatibus, aut doloremque saepe ex iste rem distinctio veniam doloribus aliquid dolor."
+        },
+        {
+            image: "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100",
+            date: "Jan 2020",
+            heading: "An Amazing website",
+            content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ullam ipsam. At cupiditate accusantium optio repudiandae pariatur fugiat necessitatibus, aut doloremque saepe ex iste rem distinctio veniam doloribus aliquid dolor."
+        },
+        {
+            image: "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100",
+            date: "Jan 2019",
+            heading: "An Amazing website",
+            content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ullam ipsam. At cupiditate accusantium optio repudiandae pariatur fugiat necessitatibus, aut doloremque saepe ex iste rem distinctio veniam doloribus aliquid dolor."
+        },
+    ]
+
+    const shouldAlignLeft = (rowId) => {
+        return rowId % 2 === 0
+    }
+
     return (
-        <div class="relative container mx-auto px-6 flex flex-col space-y-8">
+        <div className="relative container mx-auto px-6 flex flex-col space-y-8">
             <div
-                class="absolute z-0 w-2 h-full bg-white shadow-md inset-0 left-17 md:mx-auto md:right-0 md:left-0"
+                className="absolute z-0 w-2 h-full bg-white shadow-md inset-0 left-17 md:mx-auto md:right-0 md:left-0"
             ></div>
-            <div class="relative z-10">
-                <img
-                    src="https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100"
-                    alt=""
-                    class="timeline-img"
-                />
-                <div class="timeline-container">
-                    <div class="timeline-pointer" aria-hidden="true"></div>
-                    <div class="bg-white p-6 rounded-md shadow-md">
-                        <span
-                            class="font-bold text-indigo-600 text-sm tracking-wide"
-                            >Jan 2021</span
-                        >
-                        <h1 class="text-2xl font-bold pt-1">
-                            An amazing travel
-                        </h1>
-                        <p class="pt-1">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Ex iste suscipit reiciendis, perferendis vel
-                            consequuntur cupiditate ad commodi provident,
-                            sapiente veniam sed autem.
-                        </p>
+            {experiences.map((experience, index) => (
+                <div className="relative z-10">
+                    <img
+                        src={experience.image}
+                        alt=""
+                        className="timeline-img"
+                    />
+                    <div className={`timeline-container ${shouldAlignLeft(index) ? "timeline-container-left": ""}`}>
+                        <div className={`timeline-pointer ${shouldAlignLeft(index) ? "timeline-pointer-left": ""}`} aria-hidden="true"></div>
+                        <div className="bg-white p-6 rounded-md shadow-md">
+                            <span
+                                className="font-bold text-indigo-600 text-sm tracking-wide"
+                                >{experience.date}</span>
+                            <h1 className="text-2xl font-bold pt-1">{experience.heading}</h1>
+                            <p className="pt-1">{experience.content}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="relative z-10">
-                <img
-                    src="https://images.pexels.com/photos/3223552/pexels-photo-3223552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100"
-                    alt=""
-                    class="timeline-img"
-                />
-                <div class="timeline-container timeline-container-left">
-                    <div
-                        class="timeline-pointer timeline-pointer-left"
-                        aria-hidden="true"
-                    ></div>
-                    <div class="bg-white p-6 rounded-md shadow-md">
-                        <span
-                            class="font-bold text-indigo-600 text-sm tracking-wide"
-                            >Aug 2020</span
-                        >
-                        <h1 class="text-2xl font-bold pt-1">A trip far away</h1>
-                        <p class="pt-1">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Ex iste suscipit reiciendis, perferendis vel
-                            consequuntur cupiditate ad commodi provident,
-                            sapiente veniam sed
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="relative z-10">
-                <img
-                    src="https://images.pexels.com/photos/2906807/pexels-photo-2906807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100"
-                    alt=""
-                    class="timeline-img"
-                />
-                <div class="timeline-container">
-                    <div class="timeline-pointer" aria-hidden="true"></div>
-                    <div class="bg-white p-6 rounded-md shadow-md">
-                        <span
-                            class="font-bold text-indigo-600 text-sm tracking-wide"
-                            >March 2020</span
-                        >
-                        <h1 class="text-2xl font-bold pt-1">Solo Trip</h1>
-                        <p class="pt-1">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Ex iste suscipit reiciendis, perferendis vel
-                            consequuntur cupiditate ad commodi provident,
-                            sapiente veniam sed autem, perspiciatis
-                        </p>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
